fc306e463aa67db94ccb3970075ab23e458dbdba
SavioVitorAlves <saviovitor2@gmail.com>
2024-09-05 16:10:44 -0300

    refactor: update color icons

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -97,8 +97,8 @@ class _MyHomePageState extends State<MyHomePage> {
   
   Widget _getIconButton(IconData icon, Function() fn) {
     return Platform.isIOS
-        ? GestureDetector(onTap: fn, child: Icon(icon))
-        : IconButton(icon: Icon(icon), onPressed: fn);
+        ? GestureDetector(onTap: fn, child: Icon(icon,  color: Colors.white))
+        : IconButton(icon: Icon(icon,  color: Colors.white), onPressed: fn);
   }
 
   @override
