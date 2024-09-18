19e7802831ed029c8ea6004b241bc854f8968f61
SavioVitorAlves <saviovitor2@gmail.com>
2024-09-18 08:52:05 -0300

    feat: create new funcitions and refactor

--- shop/lib/components/product_item.dart
+++ shop/lib/components/product_item.dart
@@ -1,26 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/models/products.dart';
+import 'package:shop/screens/product_detail_page.dart';
+import 'package:shop/utils/app_routes.dart';
 
 class ProductItem extends StatelessWidget {
-  final Product product;
+
   
-  const ProductItem({required this.product, super.key});
+  const ProductItem({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final product = Provider.of<Product>(context, listen: false);
+
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
-        child: Image.network(
-          product.imageUrl,
-          fit: BoxFit.cover,
-        ),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
-          leading: IconButton(
-            onPressed: (){}, 
-            icon: const Icon(Icons.favorite),
-            color: Theme.of(context).colorScheme.secondary,
+          leading: Consumer<Product>(
+            builder: (ctx, product, _) => IconButton(
+              onPressed: (){
+                product.toggleFavorite();
+              }, 
+              icon:  Icon(product.isFavorite ? Icons.favorite : Icons.favorite_border),
+              color: Theme.of(context).colorScheme.secondary,
+            ),
           ),
           title: Text(
             product.title, 
@@ -30,6 +35,18 @@ class ProductItem extends StatelessWidget {
             icon: const Icon(Icons.shopping_cart ),
             color: Theme.of(context).colorScheme.secondary,
             ),
+        ),
+        child: GestureDetector(
+          child: Image.network(
+            product.imageUrl,
+            fit: BoxFit.cover,
+          ),
+          onTap: (){
+            Navigator.of(context).pushNamed(
+             AppRoutes.PRODUCT_DETAIL,
+             arguments: product,
+            );
+          },
         ),
       ),
     );
--- shop/lib/main.dart
+++ shop/lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/product_list.dart';
+import 'package:shop/screens/product_detail_page.dart';
 import 'package:shop/screens/product_overivew_page.dart';
+import 'package:shop/utils/app_routes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,18 +15,24 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed( 
-          seedColor: Colors.purple, 
-          secondary: Colors.deepOrange
+    return ChangeNotifierProvider(
+      create: (_) => ProductList(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed( 
+            seedColor: Colors.purple, 
+            secondary: Colors.deepOrange
+          ),
+          fontFamily: 'Lado',
+          useMaterial3: true,
         ),
-        fontFamily: 'Lado',
-        useMaterial3: true,
+        home: ProductOverivewPage(),
+        routes: {
+          AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetailPage(),
+        },
+        debugShowCheckedModeBanner: false,
       ),
-      home: ProductOverivewPage(),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
