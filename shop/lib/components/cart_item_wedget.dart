8c63fd7be60be4197f265a2ad9ba2d992d4c1b9b
SavioVitorAlves <saviovitor2@gmail.com>
2024-09-19 15:54:30 -0300

    docs: create widget card item

---
+++ shop/lib/components/cart_item_wedget.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:shop/models/cart_item.dart';
+
+class CartItemWedget extends StatelessWidget {
+  final CartItem cartItem;
+  const CartItemWedget({required this.cartItem, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.symmetric(
+        horizontal: 15,
+        vertical: 4
+      ),
+      child: ListTile(
+        leading: CircleAvatar(
+          backgroundColor: Theme.of(context).colorScheme.primary,
+          child: Padding(
+            padding: const EdgeInsets.all(5),
+            child: FittedBox(
+              child: Text('${cartItem.price}'),
+            ),
+          ),
+        ),
+        title: Text(cartItem.name),
+        subtitle: Text('Total: R\$ ${cartItem.price * cartItem.quantity}'),
+        trailing: Text('${cartItem.quantity}x'),
+      ),
+    );
+  }
+}
