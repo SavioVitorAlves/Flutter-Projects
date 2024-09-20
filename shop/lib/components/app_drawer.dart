a2b61717e9f44753c75f386ed004b450b2da15c6
SavioVitorAlves <saviovitor2@gmail.com>
2024-09-20 16:05:46 -0300

    docs: create drawer

---
+++ shop/lib/components/app_drawer.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:shop/utils/app_routes.dart';
+
+class AppDrawer extends StatelessWidget {
+  const AppDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          AppBar(
+            title: Text(
+              'Bem Vindo Usuario!'
+
+            ),
+            backgroundColor: Theme.of(context).colorScheme.primary,
+            centerTitle: true,
+            automaticallyImplyLeading: false,
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.shop),
+            title: Text('Loja'),
+            onTap: (){
+              Navigator.of(context).pushReplacementNamed(AppRoutes.HOME);
+            },
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.payment),
+            title: Text('Meus Pedidos'),
+            onTap: (){
+              Navigator.of(context).pushReplacementNamed(AppRoutes.ORDERS);
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
