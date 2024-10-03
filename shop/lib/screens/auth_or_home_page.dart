4ec7d800a17ee45d87611b12f8a9b49c125a47d3
SavioVitorAlves <saviovitor2@gmail.com>
2024-10-03 17:26:11 -0300

    docs: create file de verificar se o usuario esta autenticado ou nao

---
+++ shop/lib/screens/auth_or_home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/auth.dart';
+import 'package:shop/screens/auth_page.dart';
+import 'package:shop/screens/product_overivew_page.dart';
+
+class AuthOrHomePage extends StatelessWidget {
+  const AuthOrHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Auth auth = Provider.of(context);
+    return auth.isAuth ? ProductOverivewPage() : AuthPage();
+  }
+}
