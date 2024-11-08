7a15c5cc2c4c68d97c9cf3b2ff80b8b29d69353b
SavioVitorAlves <saviovitor2@gmail.com>
2024-11-07 21:08:14 -0300

    refactor: update code

--- conversor/conversor/lib/main.dart
+++ conversor/conversor/lib/main.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart' as http;
 import 'dart:async'; //nao fica esperando
 import 'dart:convert'; //transformar em json
 
-const request = "https://api.hgbrasil.com/finance?key=faf29c91";
+const request = "https://api.hgbrasil.com/finance?key=6417ae14";
 
 void main() async {
   print(await getData());
@@ -29,6 +29,17 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  String? selectedItem;
+  String? selectedItem2;
+  double inputValue = 0.0;
+  double result = 0.0;
+  Map? currencies;
+
+  double conversao(double moeda, double valor){
+    double valorConvertido = valor * moeda;
+    return valorConvertido;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -67,11 +78,111 @@ class _HomeState extends State<Home> {
                   );
                 }else{
                   // Dados carregados com sucesso, exiba os dados aqui
-                  return Text(snapshot.data!['resultado'].toString());
-                  /*return Container(
-                    color: Colors.green,
-                  );*/
+                  //return Text(snapshot.data!['resultado'].toString());
+                  currencies = snapshot.data!["results"]["currencies"];
+                  // Filtra as chaves para excluir "source" e pegar apenas as moedas
+                  final currencyKeys = currencies!.keys.where((key) => key != "source").toList();
+                  
+                  return Container(
+                    padding: EdgeInsets.all(20.0),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            DropdownButton<String>(
+                              hint: Text(
+                                'Escolha uma moeda',
+                                style: TextStyle(color: Colors.amber),
+                              ),
+                              dropdownColor: Colors.black,
+                              style: TextStyle(color: Colors.amber),
+                              value: selectedItem,
+                              onChanged: (String? newValue) {
+                                setState(() {
+                                  selectedItem = newValue;
+                                });
+                              },
+                              items: currencyKeys.map<DropdownMenuItem<String>>((dynamic key) {
+                                return DropdownMenuItem<String>(
+                                  value: key,
+                                  child: Text(
+                                    currencies![key]["name"],
+                                    style: TextStyle(color: Colors.amber),
+                                  ),
+                                );
+                              }).toList(),
+                            ),
+                            SizedBox(width: 20),
+                            DropdownButton<String>(
+                              hint: Text(
+                                'Escolha uma moeda',
+                                style: TextStyle(color: Colors.amber),
+                              ),
+                              dropdownColor: Colors.black,
+                              style: TextStyle(color: Colors.amber),
+                              value: selectedItem2,
+                              onChanged: (String? newValue) {
+                                setState(() {
+                                  selectedItem2 = newValue;
+                                });
+                              },
+                              items: currencyKeys.map<DropdownMenuItem<String>>((dynamic key) {
+                                return DropdownMenuItem<String>(
+                                  value: key,
+                                  child: Text(
+                                    currencies![key]["name"],
+                                    style: TextStyle(color: Colors.amber),
+                                  ),
+                                );
+                              }).toList(),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 20),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: TextField(
+                                style: TextStyle(color: Colors.amber),
+                                decoration: InputDecoration(
+                                  labelText: "Valor",
+                                  labelStyle: TextStyle(color: Colors.amber),
+                                  border: OutlineInputBorder(),
+                                ),
+                                keyboardType: TextInputType.number,
+                                
+                              ),
+                            ),
+                            SizedBox(width:  10),
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.amber,
+                                fixedSize: Size(double.infinity, 56),
+                                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(3))
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  result = conversao(currencies![selectedItem2!]["buy"], inputValue);
+                                });
+                              }, 
+                              child: Icon(Icons.money, color: Colors.white, size: 45,)
+                            )
+                          ],
+                        ),
+                        SizedBox(height: 20),
+                        if (selectedItem != null && selectedItem2 != null)
+                          
+                          Text(
+                            "$inputValue $selectedItem = $result $selectedItem2",
+                            style: TextStyle(color: Colors.amber, fontSize: 20),
+                          ),
+                      ],
+                    ),
+                  );
                 }
+              default:
+                return Container();
             }
           }),
     );
